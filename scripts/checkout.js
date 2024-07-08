6f28f72d8f649ceec4b0af094d55e0e440afecf5
manasviwalia2812 <manasviwalia@gmail.com>
2024-07-09 05:11:15 +0530

    delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -15,8 +15,6 @@ cart.forEach((cartItem)=>{
       matchingProduct=product;
     }
   });
-
-  console.log(matchingProduct);
 
   cartSummaryHTML+=`
     <div class="cart-item-container">
@@ -44,7 +42,7 @@ cart.forEach((cartItem)=>{
               <span class="update-quantity-link link-primary">
                 Update
               </span>
-              <span class="delete-quantity-link link-primary">
+              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
@@ -63,7 +61,7 @@ cart.forEach((cartItem)=>{
                   Tuesday, June 21
                 </div>
                 <div class="delivery-option-price">
-                  FREE Shipping
+                  FREE Shipping 
                 </div>
               </div>
             </div>
@@ -102,4 +100,11 @@ cart.forEach((cartItem)=>{
 
 document.querySelector('.js-order-summary').innerHTML= cartSummaryHTML;
 
-console.log(cartSummaryHTML);
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+    const productId= link.dataset.productId        //kebab case to camelCase
+    removeFromCart(productId);
+    console.log(cart);
+    });
+  })
