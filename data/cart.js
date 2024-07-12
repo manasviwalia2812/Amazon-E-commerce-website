dfe7ec62fa10d118ecddc030f0e86457d5b50915
manasviwalia2812 <manasviwalia@gmail.com>
2024-07-13 02:47:06 +0530

    learnt promises and callbacks

--- data/cart.js
+++ data/cart.js
@@ -69,5 +69,17 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 
   matchingItem.deliveryOptionId = deliveryOptionId;
 
-  saveToStorage();
+  saveToStorage(); 
+}
+
+export function loadCart(fun){
+  const xhr= new XMLHttpRequest();
+
+  xhr.addEventListener('load',()=>{
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open('GET','https://supersimplebackend.dev/cart');
+  xhr.send();
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,48 @@ import { renderPaymentSummary} from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
-loadProducts(()=>{
+Promise.all([
+  new Promise((resolve)=>{              
+    loadProducts(()=>{
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  })
+]).then((values)=>{
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve)=>{                //promises. keep the code flat. only run a function when the first one is finished
+  loadProducts(()=>{
+    resolve('value1');
+  });
+}).then((value)=>{
+  console.log(value);
+  return new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  });
+}).then(()=>{
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
+loadProducts(()=>{                //callbacks. become more and more nested if we have many callbacks
+  loadCart(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+  })
+});
+*/
